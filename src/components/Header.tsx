698f10af92a0392ddbf475f9fbce647d1b8c9c72
Luciano Prado <lucianobatuta@live.com>
2023-09-18 02:55:09 -0300

    Cria um header para a página de carteira

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { ReduxState } from '../types';
+
 function Header() {
-  return <div>Header</div>;
+  const userEmail = useSelector((state: ReduxState) => state.user.email);
+
+  return (
+    <header>
+      <div data-testid="email-field">
+        E-mail:
+        {' '}
+        {userEmail}
+      </div>
+
+      <div data-testid="total-field">Total: 0</div>
+
+      <div data-testid="header-currency-field">BRL</div>
+    </header>
+  );
 }
 
 export default Header;
